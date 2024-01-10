225eb96ad847574d15e505bdfdb32a6b7bb1c9bf
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-01-10 09:54:34 -0500

    Update hash method

--- main.rb
+++ main.rb
@@ -1,9 +1,17 @@
 class HashMap
     def initialize
-        
+        @buckets =  Array.new(16)
     end
 
-    def hash(name)
-        name[0]
+
+    def hash(string)
+        hash_code = 0
+        prime_number = 31
+      
+        string.each_char { |char| hash_code = prime_number * hash_code + char.ord }
+      
+        hash_code
     end
+
+    
 end
