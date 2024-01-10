ea59e0ca980ad9eaa087bbe52fabe4d639e746c4
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-01-10 12:54:47 -0500

    Implement set method

--- main.rb
+++ main.rb
@@ -1,3 +1,5 @@
+require_relative "node"
+
 class HashMap
     def initialize
         @buckets =  Array.new(16)
@@ -13,5 +15,21 @@ class HashMap
         hash_code
     end
 
+    def set(key, value)
+        index = hash(key) % 16
+        @buckets[index] = Node.new(key, value)
+    end
+
+    def capacity
+        @buckets.length
+    end
+
+    # return the amount of buckets that is not nil
+    def amount_buckets_filled
+        @buckets.count { |x| !x.nil? }
+    end
+
     
 end
+
+
